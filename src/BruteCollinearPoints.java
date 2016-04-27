26c4b9321904072ddcf541792640fe31497cb1a9
zhicheng sun <szc1523@163.com>
2016-04-27 00:08:52 -0700

    initial things

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,15 @@
+/* 
+ * by Sun Zhicheng 
+ * for coursera course Algorithms 
+ * week 3 Assignment: Collinear Point
+ */
+
+
+public class BruteCollinearPoints {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
